a99aa1c61f3899df5cfc97e9f426f3279ff7d7af
Collins Gikungu <gikungu2002@gmail.com>
2026-05-23 20:58:49 +0300

    feat: integrate react-toastify for enhanced notification display in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,35 @@
 import Dashboard from './pages/Dashboard';
 
+import {
+ ToastContainer
+} from 'react-toastify';
+
+import
+'react-toastify/dist/ReactToastify.css';
+
 function App() {
-  return (
-    <div>
-      <Dashboard />
-    </div>
-  );
+
+ return (
+
+  <>
+
+   <Dashboard />
+
+   <ToastContainer
+    position="top-right"
+    autoClose={2500}
+    hideProgressBar={false}
+    newestOnTop
+    closeOnClick
+    pauseOnHover
+    draggable
+    theme="colored"
+   />
+
+  </>
+
+ );
+
 }
 
 export default App;
