bec045680709b308e772142f025a891488b11fb5
Collins Gikungu <gikungu2002@gmail.com>
2026-06-20 19:55:11 +0300

    feat: add analytics chart components

--- frontend/src/pages/Analytics.jsx
+++ frontend/src/pages/Analytics.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import DistanceTrendChart from "../components/DistanceTrendChart";
+import CaloriesTrendChart from "../components/CaloriesTrendChart";
+import DurationTrendChart from "../components/DurationTrendChart";
+import PerformanceCorrelationChart from "../components/PerformanceCorrelationChart";
+import WorkoutConsistencyChart from "../components/WorkoutConsistencyChart";
+import WorkoutIntensityDistributionChart from "../components/WorkoutIntensityDistributionChart";
 
 const Analytics = () => {
   return (
