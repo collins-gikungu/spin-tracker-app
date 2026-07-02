d786157251d075182864154a06bb0bedc3a93a4a
Collins Gikungu <gikungu2002@gmail.com>
2026-07-02 16:20:24 +0300

    fix(api.js): revert baseURL to hardcoded value for API endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://spin-tracker-api.onrender.com/api",
 });
 
 API.interceptors.request.use(
